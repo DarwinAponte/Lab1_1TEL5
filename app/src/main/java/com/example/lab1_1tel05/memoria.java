ad6fb5434e062012faeac0fe7ba71603ed743e17
NicoG <guillen.nicole@pucp.edu.pe>
2022-09-02 09:25:35 -0500

    memoria_pt1

---
+++ app/src/main/java/com/example/lab1_1tel05/memoria.java
@@ -0,0 +1,14 @@
+package com.example.lab1_1tel05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class memoria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_memoria);
+    }
+}
