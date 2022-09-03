bd3b25e5e1993a0f214c625629ff9820a5d2114b
NicoG <guillen.nicole@pucp.edu.pe>
2022-09-02 22:02:42 -0500

    correciones memoria

--- app/src/main/java/com/example/lab1_1tel05/memoria.java
+++ app/src/main/java/com/example/lab1_1tel05/memoria.java
@@ -2,14 +2,11 @@ package com.example.lab1_1tel05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
