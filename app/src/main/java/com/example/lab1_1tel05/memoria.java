20dbee87078b1512dc834f4e52acc4df7b4bb5b9
katherine <a20181823@pucp.edu.pe>
2022-09-03 10:59:18 -0500

    Memoria

--- app/src/main/java/com/example/lab1_1tel05/memoria.java
+++ app/src/main/java/com/example/lab1_1tel05/memoria.java
@@ -271,115 +271,102 @@ public class memoria extends AppCompatActivity {
     }
 
     public void nuevoJuego(View view){
-        Button nuevo = findViewById(R.id.buttonNuevo);
-        nuevo.setOnClickListener(new View.OnClickListener(){
+        ArrayList<String> letras = new ArrayList();
+        letras.add("A");
+        letras.add("A");
+        letras.add("B");
+        letras.add("B");
+        letras.add("C");
+        letras.add("C");
+        letras.add("D");
+        letras.add("D");
+        letras.add("E");
+        letras.add("E");
+        letras.add("F");
+        letras.add("F");
+        letras.add("G");
+        letras.add("G");
+        letras.add("H");
+        letras.add("H");
+        Random rndm = new Random();
+        Collections.shuffle(letras, rndm);
 
+        Button btn1 = findViewById(R.id.button1);
+        btn1.setText(String.valueOf(letras.get(0)));
+        Button btn2= findViewById(R.id.button2);
+        btn2.setText(String.valueOf(letras.get(1)));
+        Button btn3= findViewById(R.id.button3);
+        btn3.setText(String.valueOf(letras.get(2)));
+        Button btn4= findViewById(R.id.button4);
+        btn4.setText(String.valueOf(letras.get(3)));
+        Button btn5= findViewById(R.id.button5);
+        btn5.setText(String.valueOf(letras.get(4)));
+        Button btn6= findViewById(R.id.button6);
+        btn6.setText(String.valueOf(letras.get(5)));
+        Button btn7= findViewById(R.id.button7);
+        btn7.setText(String.valueOf(letras.get(6)));
+        Button btn8= findViewById(R.id.button8);
+        btn8.setText(String.valueOf(letras.get(7)));
+        Button btn9= findViewById(R.id.button9);
+        btn9.setText(String.valueOf(letras.get(8)));
+        Button btn10= findViewById(R.id.button10);
+        btn10.setText(String.valueOf(letras.get(9)));
+        Button btn11= findViewById(R.id.button11);
+        btn11.setText(String.valueOf(letras.get(10)));
+        Button btn12= findViewById(R.id.button12);
+        btn12.setText(String.valueOf(letras.get(11)));
+        Button btn13= findViewById(R.id.button13);
+        btn13.setText(String.valueOf(letras.get(12)));
+        Button btn14= findViewById(R.id.button14);
+        btn14.setText(String.valueOf(letras.get(13)));
+        Button btn15= findViewById(R.id.button15);
+        btn15.setText(String.valueOf(letras.get(14)));
+        Button btn16= findViewById(R.id.button16);
+        btn16.setText(String.valueOf(letras.get(15)));
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onClick(View v){
-                ArrayList<String> letras = new ArrayList();
-                Random rndm = new Random();
-                letras.add("A");
-                letras.add("A");
-                letras.add("B");
-                letras.add("B");
-                letras.add("C");
-                letras.add("C");
-                letras.add("D");
-                letras.add("D");
-                letras.add("E");
-                letras.add("E");
-                letras.add("F");
-                letras.add("F");
-                letras.add("G");
-                letras.add("G");
-                letras.add("H");
-                letras.add("H");
-                Collections.shuffle(letras, rndm);
-                Log.d("lista",""+letras);
-
+            public void run() {
                 Button btn1 = findViewById(R.id.button1);
-                btn1.setText(String.valueOf(letras.get(0)));
-
-
-
-
+                btn1.setText("-");
                 Button btn2= findViewById(R.id.button2);
-                btn2.setText(String.valueOf(letras.get(1)));
+                btn2.setText("-");
                 Button btn3= findViewById(R.id.button3);
-                btn3.setText(String.valueOf(letras.get(2)));
+                btn3.setText("-");
                 Button btn4= findViewById(R.id.button4);
-                btn4.setText(String.valueOf(letras.get(3)));
+                btn4.setText("-");
                 Button btn5= findViewById(R.id.button5);
-                btn5.setText(String.valueOf(letras.get(4)));
+                btn5.setText("-");
                 Button btn6= findViewById(R.id.button6);
-                btn6.setText(String.valueOf(letras.get(5)));
+                btn6.setText("-");
                 Button btn7= findViewById(R.id.button7);
-                btn7.setText(String.valueOf(letras.get(6)));
+                btn7.setText("-");
                 Button btn8= findViewById(R.id.button8);
-                btn8.setText(String.valueOf(letras.get(7)));
+                btn8.setText("-");
                 Button btn9= findViewById(R.id.button9);
-                btn9.setText(String.valueOf(letras.get(8)));
+                btn9.setText("-");
                 Button btn10= findViewById(R.id.button10);
-                btn10.setText(String.valueOf(letras.get(9)));
+                btn10.setText("-");
                 Button btn11= findViewById(R.id.button11);
-                btn11.setText(String.valueOf(letras.get(10)));
+                btn11.setText("-");
                 Button btn12= findViewById(R.id.button12);
-                btn12.setText(String.valueOf(letras.get(11)));
+                btn12.setText("-");
                 Button btn13= findViewById(R.id.button13);
-                btn13.setText(String.valueOf(letras.get(12)));
+                btn13.setText("-");
                 Button btn14= findViewById(R.id.button14);
-                btn14.setText(String.valueOf(letras.get(13)));
+                btn14.setText("-");
                 Button btn15= findViewById(R.id.button15);
-                btn15.setText(String.valueOf(letras.get(14)));
+                btn15.setText("-");
                 Button btn16= findViewById(R.id.button16);
-                btn16.setText(String.valueOf(letras.get(15)));
-
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Button btn1 = findViewById(R.id.button1);
-                        btn1.setText("-");
-                        Button btn2= findViewById(R.id.button2);
-                        btn2.setText("-");
-                        Button btn3= findViewById(R.id.button3);
-                        btn3.setText("-");
-                        Button btn4= findViewById(R.id.button4);
-                        btn4.setText("-");
-                        Button btn5= findViewById(R.id.button5);
-                        btn5.setText("-");
-                        Button btn6= findViewById(R.id.button6);
-                        btn6.setText("-");
-                        Button btn7= findViewById(R.id.button7);
-                        btn7.setText("-");
-                        Button btn8= findViewById(R.id.button8);
-                        btn8.setText("-");
-                        Button btn9= findViewById(R.id.button9);
-                        btn9.setText("-");
-                        Button btn10= findViewById(R.id.button10);
-                        btn10.setText("-");
-                        Button btn11= findViewById(R.id.button11);
-                        btn11.setText("-");
-                        Button btn12= findViewById(R.id.button12);
-                        btn12.setText("-");
-                        Button btn13= findViewById(R.id.button13);
-                        btn13.setText("-");
-                        Button btn14= findViewById(R.id.button14);
-                        btn14.setText("-");
-                        Button btn15= findViewById(R.id.button15);
-                        btn15.setText("-");
-                        Button btn16= findViewById(R.id.button16);
-                        btn16.setText("-");
-                    }
-                }, 1000);
-
+                btn16.setText("-");
             }
-
-
-        });
-
+        }, 1000);
 
     }
+
+
+
 
 
 
