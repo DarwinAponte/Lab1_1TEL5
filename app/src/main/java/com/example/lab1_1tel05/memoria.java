edb26a4b0c2998de0a61cb0bc53d5374c249b383
NicoG <guillen.nicole@pucp.edu.pe>
2022-09-02 22:35:29 -0500

    se ven letras

--- app/src/main/java/com/example/lab1_1tel05/memoria.java
+++ app/src/main/java/com/example/lab1_1tel05/memoria.java
@@ -2,6 +2,7 @@ package com.example.lab1_1tel05;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -41,36 +42,190 @@ public class memoria extends AppCompatActivity {
 
         Button btn1 = findViewById(R.id.button1);
         btn1.setText(String.valueOf(letras.get(0)));
+
+        btn1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn1.setText(String.valueOf(letras.get(0)));
+            }
+
+
+        });
+
         Button btn2= findViewById(R.id.button2);
         btn2.setText(String.valueOf(letras.get(1)));
+
+        btn2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn2.setText(String.valueOf(letras.get(1)));
+            }
+
+
+        });
+
         Button btn3= findViewById(R.id.button3);
         btn3.setText(String.valueOf(letras.get(2)));
+
+        btn3.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn3.setText(String.valueOf(letras.get(2)));
+            }
+
+        });
+
         Button btn4= findViewById(R.id.button4);
         btn4.setText(String.valueOf(letras.get(3)));
+
+        btn4.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn4.setText(String.valueOf(letras.get(3)));
+            }
+
+
+        });
+
+
         Button btn5= findViewById(R.id.button5);
         btn5.setText(String.valueOf(letras.get(4)));
+
+        btn5.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn5.setText(String.valueOf(letras.get(4)));
+            }
+
+
+        });
+
+
         Button btn6= findViewById(R.id.button6);
         btn6.setText(String.valueOf(letras.get(5)));
+        btn6.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn6.setText(String.valueOf(letras.get(5)));
+            }
+
+
+        });
+
+
         Button btn7= findViewById(R.id.button7);
         btn7.setText(String.valueOf(letras.get(6)));
+        btn7.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn7.setText(String.valueOf(letras.get(6)));
+            }
+
+
+        });
+
         Button btn8= findViewById(R.id.button8);
         btn8.setText(String.valueOf(letras.get(7)));
+        btn8.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn8.setText(String.valueOf(letras.get(7)));
+            }
+
+
+        });
+
         Button btn9= findViewById(R.id.button9);
         btn9.setText(String.valueOf(letras.get(8)));
+        btn9.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn9.setText(String.valueOf(letras.get(8)));
+            }
+
+
+        });
+
         Button btn10= findViewById(R.id.button10);
         btn10.setText(String.valueOf(letras.get(9)));
+        btn10.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn10.setText(String.valueOf(letras.get(9)));
+            }
+
+
+        });
+
         Button btn11= findViewById(R.id.button11);
         btn11.setText(String.valueOf(letras.get(10)));
+        btn11.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn11.setText(String.valueOf(letras.get(10)));
+            }
+        });
+
         Button btn12= findViewById(R.id.button12);
         btn12.setText(String.valueOf(letras.get(11)));
+        btn12.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn12.setText(String.valueOf(letras.get(11)));
+            }
+        });
+
         Button btn13= findViewById(R.id.button13);
         btn13.setText(String.valueOf(letras.get(12)));
+        btn13.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn13.setText(String.valueOf(letras.get(12)));
+            }
+        });
+
         Button btn14= findViewById(R.id.button14);
         btn14.setText(String.valueOf(letras.get(13)));
+        btn14.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn14.setText(String.valueOf(letras.get(13)));
+            }
+        });
+
         Button btn15= findViewById(R.id.button15);
         btn15.setText(String.valueOf(letras.get(14)));
+        btn15.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn15.setText(String.valueOf(letras.get(14)));
+            }
+        });
+
         Button btn16= findViewById(R.id.button16);
         btn16.setText(String.valueOf(letras.get(15)));
+        btn16.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                btn16.setText(String.valueOf(letras.get(15)));
+            }
+        });
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -144,6 +299,10 @@ public class memoria extends AppCompatActivity {
 
                 Button btn1 = findViewById(R.id.button1);
                 btn1.setText(String.valueOf(letras.get(0)));
+
+
+
+
                 Button btn2= findViewById(R.id.button2);
                 btn2.setText(String.valueOf(letras.get(1)));
                 Button btn3= findViewById(R.id.button3);
